d1908bdfda8af1f8fe37abefdfc25ca1e6e5ff5c
sansdev <sansdev.malang@gmail.com>
2026-08-02 15:15:38 +0700

    chore(bonus): rename Rekapan to Rekap in PDF title tag

--- resources/views/bonus-reports/export-pdf.blade.php
+++ resources/views/bonus-reports/export-pdf.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Rekapan Bonus Kehadiran</title>
+    <title>Rekap Bonus Kehadiran</title>
     <style>
         body {
             font-family: Arial, sans-serif;
