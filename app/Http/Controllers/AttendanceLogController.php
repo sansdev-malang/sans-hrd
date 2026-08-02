6bcb31be1a621021da60fa4ec0ba54b693d1bbb5
sansdev <sansdev.malang@gmail.com>
2026-08-02 14:04:29 +0700

    fix(attendance): remove L and use red dash for Libur

--- app/Http/Controllers/AttendanceLogController.php
+++ app/Http/Controllers/AttendanceLogController.php
@@ -423,8 +423,8 @@ class AttendanceLogController extends Controller
                         $cellValue = $detail['leave_type'] ?? 'IZIN';
                         $sheet->getStyle($colLetter . $row)->getFont()->setColor(new \PhpOffice\PhpSpreadsheet\Style\Color(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE));
                     } elseif ($detail['status'] === 'Libur') {
-                        $cellValue = 'L';
-                        $sheet->getStyle($colLetter . $row)->getFont()->setColor(new \PhpOffice\PhpSpreadsheet\Style\Color('FF9CA3AF'));
+                        $cellValue = '-';
+                        $sheet->getStyle($colLetter . $row)->getFont()->setColor(new \PhpOffice\PhpSpreadsheet\Style\Color(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_RED));
                     } elseif ($detail['status'] === 'Off') {
                         $cellValue = 'OFF';
                         $sheet->getStyle($colLetter . $row)->getFont()->setColor(new \PhpOffice\PhpSpreadsheet\Style\Color('FF9CA3AF'));
